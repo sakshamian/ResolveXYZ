f51eae2f1c691745710824f26f02621e7d70af13
Saskham Negi <negisaksham363@gmail.com>
2024-12-21 13:00:42 +0530

    Code refactoring

---
+++ nyr-BE/db/database.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func Connect() {
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("mongodb+srv://sakshamnegi:pass@nyrhub.23g4a.mongodb.net/?retryWrites=true&w=majority&appName=nyrhub").SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+		panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+}
---
+++ nyr-BE/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"nyr/db"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println("Error loading .env file", err)
+	}
+
+	r := gin.Default()
+	db.Connect()
+
+	
+	//getting PORT from env file
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9090"
+	}
+
+	if err := r.Run(":" + port); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
+}
