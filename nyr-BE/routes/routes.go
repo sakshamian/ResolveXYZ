c4f72003f0fcb593ed0635100648ffde0880612c
ridhimaz <ridhimakohli19@gmail.com>
2024-12-22 14:00:25 +0530

    implemented resolution routes

--- nyr-BE/routes/routes.go
+++ nyr-BE/routes/routes.go
@@ -13,4 +13,11 @@ func InitRoutes(router *gin.Engine) {
 	// auth routes
 	router.GET("/auth/google", controllers.GoogleLogin)
 	router.GET("/auth/google/callback", controllers.GoogleCallback)
+
+	// resolution routes
+	router.POST("/resolution", controllers.CreateResolution)
+	router.POST("/likes", controllers.ToggleLikeResolution)
+	router.POST("/comments", controllers.CreateComment)
+	router.GET("/", controllers.GetResolutions)
+	router.GET("/resolution/:id", controllers.GetResolutionByID)
 }
