c1854ec38d3ac751bf12036ca0adcd88aefce214
Saskham Negi <negisaksham363@gmail.com>
2024-12-28 20:21:07 +0530

    Removed unused variables

--- nyr-FE/src/Pages/User/MyResolutions.tsx
+++ nyr-FE/src/Pages/User/MyResolutions.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import ResolutionCard from '../../Components/Card/ResolutionCard';
 import { useEffect, useState } from 'react';
 import { fetchMyResolutions } from '../../services/api';
